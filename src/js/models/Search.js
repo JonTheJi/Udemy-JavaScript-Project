544c168501c4bc2138d66ebe5c2cf0a4c298f9e7
JonTheJi <johnjiangtw0804@gmail.com>
2018-08-30 22:25:49 -0700

    First commit for the udemy java project, just done with the fist api call in Search class. Done with MVC concept, Model/Search.js partly done, and now doing some searchView in the controller.

---
+++ src/js/models/Search.js
@@ -0,0 +1,21 @@
+// api key e974489e001fdd461e3eecdfadb47edf
+// URL http://food2fork.com/api/search
+import axios from 'axios';
+
+export default class Search {
+  constructor (query) {
+    this.query = query;
+  }
+
+  async getResults() {
+    const key = 'e974489e001fdd461e3eecdfadb47edf';
+    try {
+      const res = await axios(`http://food2fork.com/api/search?key=${key}&
+        q=${this.query}`);
+      this.result = res.data.recipes;
+      console.log(this.result);
+    } catch (error) {
+      alert(error);
+    }
+  }
+}
